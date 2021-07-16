4f25c6fe21c9c3155eb10c2949cf12c18380e2ce
Abhinav Magadi <abhinavmagadi@Abhinavs-MacBook-Air.local>
2021-07-16 18:51:09 +0530

    innerhtml

--- oncall.js
+++ oncall.js
@@ -1,38 +1,3 @@
-var pricesdict = {
-    "JKTyre":5000,
-    "MRFTyre":5400,
-    "ScorpionTyre":6000,
-    "YokohomaTyre":6500,
-    "AlloyWheel":10000,
-    "HeavyWheel":12000,
-    "PedaWheel":13000,
-    "RacingWheel":16000,
-    "HardSuspension":16000,
-    "SoftSuspension":17000,
-    "MediumSuspension":20000,
-    "Race_Car_Suspension":25000,
-    "XenonHeadlight":14000,
-    "ProjectorHeadlight":9000,
-    "HalogenHeadlight":7000,
-    "LEDHeadlight":10000,
-    "FlatBottom":9000,
-    "3Spoke":6000,
-    "4Spoke":5000,
-    "RaceingSteering":12000,
-    "ORVM":6000,
-    "IRVM":3000,
-    "VerticalORVM":4000,
-    "ORVM-Indicators":8000,
-    "DiskBrakes":14000,
-    "DoublediskBrakes":20000,
-    "DrumBrakes":10000,
-    "ABS":24000,
-    "SparkPlug":4000,
-    "AirFilter":2000,
-    "EngineOil":5000,
-    "Coolant":3000
-}
-
 var totalsum = 0;
 var finalstack = [];
 
@@ -49,7 +14,7 @@ function oncalls(iden){
 
 function sends(){
     localStorage.setItem("final",JSON.stringify(finalstack));
-    document.write(JSON.stringify(finalstack));
+    localStorage.setItem("price",JSON.stringify(pricesdict));
 }
 
 
